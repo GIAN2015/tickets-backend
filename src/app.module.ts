706c8260a361c20671896a0704889befc5c4b3d8
Gian Sinarahua <gian.sinarahua@danyris.com>
2025-09-16 10:13:13 -0500

    last commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { TicketsModule } from './tickets/tickets.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { EmpresasModule } from './empresas/empresas.module'; 
-import { APP_GUARD } from '@nestjs/core';
-
-import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { EmpresasModule } from './empresas/empresas.module';
 import { MailModule } from './mail.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // para usar process.env en cualquier lado
+    }),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // 🔹 necesario porque Render usa certificados self-signed
+      },
+
     }),
     MailModule,
     AuthModule,
@@ -23,6 +32,5 @@ import { MailModule } from './mail.module';
     TicketsModule,
     EmpresasModule,
   ],
-
 })
-export class AppModule {}
+export class AppModule { }
